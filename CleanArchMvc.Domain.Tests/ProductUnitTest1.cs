e279ca74c60e41fb017fc8b4fd85eab20d45226e
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-10-02 18:04:31 -0300

    34. Testes de unidade para a entidade Product

---
+++ CleanArchMvc.Domain.Tests/ProductUnitTest1.cs
@@ -0,0 +1,88 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTest1
+    {
+        [Fact]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_NegativeIdValue_DomainExcpetionInvalidId()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Fact]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, 
+                "product image with a very long name that exceeds the maximum allowed length of 250 characters. " +
+                "This image name is intentionally made excessively long to trigger the domain exception validation " +
+                "and ensure that the validation logic correctly identifies and handles cases where the image name " +
+                "exceeds the specified limit.");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid image name, too long, maximum 250 characters");
+        }
+
+        [Fact]
+        public void CreateProduct_WithNullImage_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_WithEmptyImage_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact]
+        public void CreateProduct_InvalidPriceValue_DomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid price value");
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value)
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, value, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid stock value");
+        }
+    }
+}
