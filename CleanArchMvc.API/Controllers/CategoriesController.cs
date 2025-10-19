731e7f732aa0e89733581ead445f82f1319a08a8
brunooyamada <51134391+brunooyamada@users.noreply.github.com>
2025-10-18 21:40:37 -0300

    93. HttpPut - Alterando uma Categoria existente

--- CleanArchMvc.API/Controllers/CategoriesController.cs
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -49,5 +49,19 @@ namespace CleanArchMvc.API.Controllers
             return new CreatedAtRouteResult("GetCategory", new {id = categoryDto.Id}, 
                 categoryDto);
         }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id,[FromBody] CategoryDTO categoryDto)
+        {
+            if (id != categoryDto.Id)
+                return BadRequest();
+
+            if (categoryDto == null)
+                return BadRequest();
+
+            await _categoryService.Update(categoryDto);
+
+            return Ok(categoryDto);
+        }
     }
 }
